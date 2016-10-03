219385373c1f98d17e30be6d3ff23884747e4e5e
oliveira-daniel <lacrimaopera@gmail.com>
2016-10-03 11:59:32 -0300

    Inicio do projeto

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    [SerializeField]
+    private float applyForce;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Aplicar a força no objeto
+        rb.AddForce(new Vector2(
+            -Input.GetAxis("Horizontal") * applyForce,
+            -Input.GetAxis("Vertical") * applyForce
+        ), ForceMode2D.Force);
+
+    }
+
+    void FixedUpdate()
+    {
+
+    }
+}
