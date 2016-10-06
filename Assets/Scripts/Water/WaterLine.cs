94a9fa542c87cd5807cf314dc081c1a0001e1b9f
oliveira-daniel <lacrimaopera@gmail.com>
2016-10-06 12:21:10 -0300

    Testes com click do mouse

--- Assets/Scripts/Water/WaterLine.cs
+++ Assets/Scripts/Water/WaterLine.cs
@@ -126,4 +126,16 @@ public class WaterLine : MonoBehaviour
         }
 
     }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+            Debug.Log("Pressed left click.");
+
+        if (Input.GetMouseButtonDown(1))
+            Debug.Log("Pressed right click.");
+
+        if (Input.GetMouseButtonDown(2))
+            Debug.Log("Pressed middle click.");
+    }
 }
